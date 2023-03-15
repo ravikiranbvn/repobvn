009e08d3ee8b9d17828760877a43b5db3167ce2d
Venkata Naga Ravikiran Bulusu <ravi.has.kiran@gmail.com>
2023-03-15 15:06:32 +0100

    Update ThreadPool.hpp

--- cpp/projects/Scheduler/ThreadPool.hpp
+++ cpp/projects/Scheduler/ThreadPool.hpp
@@ -1,1 +1,61 @@
+#pragma once
 
+#include <future>
+#include <functional>
+#include <thread>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <vector>
+#include <iostream>
+
+template<typename F, typename... Args>
+using JobReturnType = typename std::result_of<F(Args...)>::type;
+
+enum class Priority : size_t
+{
+   Normal,
+   High,
+   Critical
+};
+
+class ThreadPool
+{
+public:
+       ThreadPool(size_t threadCount = std::thread::hardware_concurrency());	
+       ThreadPool(ThreadPool&&) = default;
+       ThreadPool& operator=(ThreadPool&&) = default;
+	
+       ~ThreadPool();
+       ThreadPool(const ThreadPool&) = delete;
+       ThreadPool& operator=(const ThreadPool&) = delete;
+       	
+       template <typename F, typename... Args>
+       auto startWithPriority(Priority priority, F&& f, Args&&... args) -> std::future<JobReturnType<F, Args...>>
+       {
+          auto job = std::make_shared<std::packaged_task<JobReturnType<F, Args...>()>>
+			(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+	   addTask(priority, [job] { (*job)();});
+	   return job->get_future();
+       }
+       
+       template <typename F, typename... Args>
+       auto start(F&& f, Args&&... args) -> std::future<JobReturnType<F, Args...>>
+       {
+	    return startWithPriority(Priority::Normal, std::forward<F>(f), std::forward<Args>(args)...);
+       }
+
+       void shutDown();
+
+private:
+       void initialize(size_t threadCount);	
+       void addTask(Priority priority, std::function<void()> job);
+       void handlerTask();
+
+       std::mutex m_mutex;
+       std::condition_variable m_cv;
+       std::vector<std::thread> m_workers;
+       std::map<Priority, std::queue<std::function<void()>>, std::greater<Priority>> m_tasksByPriority;
+};
