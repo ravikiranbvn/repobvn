67d9f00642d1916d4929b3c248e85ab69e603c3f
ravikiranbvn <ravi.has.kiran@gmail.com>
2023-07-15 00:47:37 +0200

    testing new setup on windows11 with vscode

--- cpp/stl/vs_cpp_ex/vectorEx.cpp
+++ cpp/stl/vs_cpp_ex/vectorEx.cpp
@@ -17,6 +17,6 @@ int main()
     vector<int> v1{1, 4, 9, 16};
     vector<int> v2{9, 7, 4, 9, 11};
     std::move(v2.begin(), v2.end(), std::back_inserter(v1));
-    std::cout << v1;
+    std::cout << "printing" << v1;
     return 0;
 }
